f01d49fb88b789d541737cd685eaf56df7fbf604
Ash-C-Codes-Code <138021538+Ash-C-Codes-Code@users.noreply.github.com>
2024-08-26 16:35:31 +0100

    Setting up a HTTP Get command for testing

--- map.go
+++ map.go
@@ -1,4 +1,31 @@
 // Setup HTTP Connection
-// Test using the API
+// Test using the API Open Source Routing Machine
 // Check how many calls can be made and in what timeframe
 // Check possibility of adding in an interactive map
+
+
+package main
+
+import {
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"errors"
+	"os"
+	"time"
+}
+
+const serverPort = 3333
+
+func main() {
+	requestURL := fmt.Sprintf("", serverPort)
+	res, err := http.Get(requestURL)
+	if err != nil {
+		fmt.Printf("error making http request: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("status code: %d\n", res.StatusCode)
+}
+
